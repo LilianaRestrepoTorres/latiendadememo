b0a6a840978f5f0ed87793ac3d572555fb56a080
Liliana Restrepo Torres <lilianarestrepot@gmail.com>
2016-10-22 11:09:56 -0500

    Refactored code controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [ :edit, :update, :show, :destroy ]
+
   def new
     @product = Product.new
   end
@@ -7,7 +9,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = 'The product have been saved'
-      redirect_to @product
+      redirect_to product_path(id: @product.id, it_was: 'created')
     else
       @errors = @product.errors
       render :new
@@ -15,7 +17,16 @@ class ProductsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
     @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'Product was successfully update.'
+      redirect_to product_path(id: @product.id, it_was: 'updated')
+    else
+      render :edit
+    end
   end
 
   def index
@@ -23,19 +34,22 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     flash[:notice] = 'Product was successfully destroyed.'
     redirect_to products_path
   end
 
   def show
-    @product = Product.find(params[:id])
+    @action = params[:it_was]
   end
 
   private
   #Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
     end
 end
