384e13469a8bab46e189662216c1c64ef4213cde
Liliana Restrepo Torres <lilianarestrepot@gmail.com>
2016-10-26 18:34:18 -0500

    Create root path welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :categories
 
-  get 'pages/home'
-
-    resources :products
-    resources :categories
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
 end
