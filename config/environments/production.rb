4e7ca92d48a4a006b53c05addf83cf9e0d52f5d0
Liliana Restrepo Torres <lilianarestrepot@gmail.com>
2016-11-17 20:37:38 -0500

    Add to config for sendgrid to config/environments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,16 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  #Sendgrid Configuration
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'https://store-lili.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true  
+  }
+
 end
